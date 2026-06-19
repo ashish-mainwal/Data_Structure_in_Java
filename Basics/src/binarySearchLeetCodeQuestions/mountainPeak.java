e887527eadfb060bf6124d9fb048527ab0c5a384
ashish-mainwal <ashishmainwal8@gmail.com>
2026-06-19 23:21:36 +0530

    finding peak in bitonic array

--- Basics/src/binarySearchLeetCodeQuestions/mountainPeak.java
+++ Basics/src/binarySearchLeetCodeQuestions/mountainPeak.java
@@ -2,6 +2,21 @@ package binarySearchLeetCodeQuestions;
 
 public class mountainPeak {
     public static void main(String[] args) {
-
+        int[] arr = {1,2,3,5,6,4,3,2};
+        System.out.println(arr[searchPeak(arr)]);
+    }
+    static int searchPeak(int[] arr){
+        int start = 0;
+        int end = arr.length -1;
+        while(start < end){
+            int mid = start + (end - start)/2;
+            if(arr[mid] > arr[mid + 1]){
+                end = mid;
+            }
+            else{
+                start = mid + 1;
+            }
+        }
+        return end;
     }
 }
